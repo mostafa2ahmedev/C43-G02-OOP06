25b7cdf5273c97af28515a113d878dae3d82043d
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-01-23 14:46:39 +0200

    P1 Q1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01
+
+
+            #region Question 1:  What is the primary purpose of an interface in C#? 
+            // a) To provide a way to implement multiple inheritance
+            #endregion
+
+
+            #endregion
+
+
+
+
+
         }
     }
 }
